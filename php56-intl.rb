cc6c8c842418ce1864b7f93fec3ce496478f4919
Jack Zhang <j@ckzhang.com>
2018-06-01 16:32:18 -0700

    Update php56-intl.rb

--- php56-intl.rb
+++ php56-intl.rb
@@ -11,12 +11,7 @@ class Php56Intl < AbstractPhp56Extension
   puts PHP_SRC_TARBALL
   puts PHP_CHECKSUM
   puts PHP_VERSION
-  
-  bottle do	
-    root_url "https://github.com/aurambaj/homebrew-phpintlicu521/releases/download/0.1" 	
-    sha256 "69a857e34201b3de6cadbef23bae6b323f0377a772f9160e2cbbe9d575fb939b" => :el_capitan
-    sha256 "69a857e34201b3de6cadbef23bae6b323f0377a772f9160e2cbbe9d575fb939b" => :sierra
-  end
+
   
   depends_on "icu4c521"
   
