56f742794336f47a1f45f0b9031afc6b45b161a1
Jack Zhang <j@ckzhang.com>
2018-06-01 16:30:07 -0700

    Update icu4c521.rb

--- icu4c521.rb
+++ icu4c521.rb
@@ -7,11 +7,6 @@ class Icu4c521 < Formula
   sha256 "2f4d5e68d4698e87759dbdc1a586d053d96935787f79961d192c477b029d8092"
   head 'http://source.icu-project.org/repos/icu/icu/trunk/', :using => :svn
 
-  bottle do
-    root_url "https://github.com/aurambaj/homebrew-phpintlicu521/releases/download/0.1"
-    sha256 "32e8b0718ce72f018e4c9cff3d7e8956ab5de71270ac0ae460b610cf255848de" => :el_capitan
-  end
-
   keg_only "Conflicts; see: https://github.com/Homebrew/homebrew/issues/issue/167"
 
   conflicts_with "icu4c", :because => "Differing versions of same formula"
