ae7160f8bcbf6bbae3f89348347a521d5c48cceb
Jack Zhang <j@ckzhang.com>
2018-06-01 17:10:18 -0700

    Update php56-intl.rb

--- php56-intl.rb
+++ php56-intl.rb
@@ -32,7 +32,7 @@ class Php56Intl < AbstractPhp56Extension
   end
 
   def config_file
-    super + <<-EOS.undent
+    super + <<-EOS
       ;intl.default_locale =
       ; This directive allows you to produce PHP errors when some error
       ; happens within intl functions. The value is the level of the error produced.
