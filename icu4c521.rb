cbfa68d422de8529c35dbae9b6e4c0fc58972d13
Jack Zhang <j@ckzhang.com>
2018-06-02 09:46:33 -0700

    Update icu4c521.rb

--- icu4c521.rb
+++ icu4c521.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Icu4c521 < Formula
   homepage 'http://site.icu-project.org/'
-  url 'http://download.icu-project.org/files/icu4c/53.1/icu4c-53_1-src.tgz'
-  version '53.1'
-  sha256 "6fa74fb5aac070c23eaba1711a7178fe582c59867484c5ec07c49002787a9a28"
+  url 'http://download.icu-project.org/files/icu4c/52.1/icu4c-52_1-src.tgz'
+  version '52.1'
+  sha256 "2f4d5e68d4698e87759dbdc1a586d053d96935787f79961d192c477b029d8092"
   head 'http://source.icu-project.org/repos/icu/icu/trunk/', :using => :svn
 
   keg_only "Conflicts; see: https://github.com/Homebrew/homebrew/issues/issue/167"
